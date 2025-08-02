706beaf43e9942303ebfb903ac7f83391dca8769
Nirbhay <nirbhaymarde43@gmail.com>
2025-08-02 22:46:15 +0530

    Updated geo-api base url

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
     WEATHER_API_BASE_URL: "https://api.openweathermap.org/data/2.5",
-    GEO_API_BASE_URL: "http://api.openweathermap.org/geo/1.0",
+    GEO_API_BASE_URL: "https://api.openweathermap.org/geo/1.0",
     API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
     DEFAULT_PARAMS: {
         units: "metric", 
